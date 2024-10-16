8391daee690a57e1c96288ce9ab09dd2999c8419
Christian-Kaden <christian.kaden.dev@gmail.com>
2024-10-16 22:46:29 +0200

    finished references

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+  styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
+  aggrement = false;
+  errorMessage = false;
 
+  acceptPrivacyPolicy() {
+    this.aggrement = !this.aggrement;
+    if (this.aggrement && this.errorMessage) {
+      this.errorMessage = !this.errorMessage;
+    }
+  }
+  checkAggrement() {
+    if (!this.aggrement && !this.errorMessage) {
+      this.errorMessage = !this.errorMessage;
+    }
+  }
 }
--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  styleUrl: './references.component.scss',
 })
 export class ReferencesComponent {
+  currentReference = true;
 
+  cards = [
+    {
+      text: 'Lukas has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project.',
+      author: 'H.Janisch - Team Partner',
+    },
+    {
+      text: "I had the good fortune of working with Lukas in a group project at the Developer Akademie that involved a lot of effort. He always stayed calm, cool, and focused, and made sure our team was set up for success. He's super knowledgeable, easy to work with, and I'd happily work with him again given the chance.",
+      author: 'A. Fischer - Team Partner',
+    },
+    {
+      text: 'Our project benefited enormously from Simon efficient way of working.',
+      author: 'T.Schulz - Frontend Developer',
+    },
+  ];
+
+  currentCardIndex = 0;
+
+  // Change to the previous card
+  prevCard() {
+    this.currentCardIndex =
+      (this.currentCardIndex - 1 + this.cards.length) % this.cards.length;
+  }
+
+  // Change to the next card
+  nextCard() {
+    this.currentCardIndex = (this.currentCardIndex + 1) % this.cards.length;
+  }
+
+  // Go directly to a specific card
+  goToCard(index: number) {
+    this.currentCardIndex = index;
+  }
 }
